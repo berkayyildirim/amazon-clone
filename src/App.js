e25c2810d7dc67ab50b70cac8aaa9d25f7619d0e
berkayyildirim <berkay.yildirim@yahoo.com>
2021-03-21 17:33:39 +0300

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+
+//download prettier extension for vscode
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="app">
+      <Switch>
+        <Route path="/checkout">
+          <h1>Checkout</h1>
+        </Route>
+        <Router path="/login">
+          <h1>Login</h1>
+        </Router>
+        <Route path="/">
+          <h1>Home Page</h1>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
+
+//localhost.com/
+
+//localhost.com/checkout
+
+//localhost.com/login
 
 export default App;
