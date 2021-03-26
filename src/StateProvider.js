edeed19633de062a0f6c59fbfaf9f1df678df5cc
berkayyildirim <berkay.yildirim@yahoo.com>
2021-03-26 12:41:12 +0300

    setup add to basket button

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -10,11 +10,13 @@ export const StateContext = createContext();
 
 // arrow function'da eğer object açarsan içinde bir şey return etmen gerek. Object açtın ama retunr koymazsan hiçbir şey vermez. Eğer object açmaz boş bırakırsan, direk ne yazıyorsa onu verir.
 
-export const StateProvider = ({ reducer, initialState, children }) => (
+export const StateProvider = ({ reducer, initialState, children }) => {
+    return (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
-);
+    );
+};
 
 // This is how we use it inside of a component
 export const useStateValue = () => useContext(StateContext);
