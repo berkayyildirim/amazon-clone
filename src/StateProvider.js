52f9998d7f1e1817424d1f5f6a14b3a953209d08
berkayyildirim <berkay.yildirim@yahoo.com>
2021-03-26 12:38:40 +0300

    implement react context api

---
+++ src/StateProvider.js
@@ -0,0 +1,20 @@
+// setup data layer
+// we need this to track the basket
+
+import React, {createContext, useContext, useReducer} from "react";
+
+// This is the data layer
+export const StateContext = createContext();
+
+// Build a provider
+
+// arrow function'da eğer object açarsan içinde bir şey return etmen gerek. Object açtın ama retunr koymazsan hiçbir şey vermez. Eğer object açmaz boş bırakırsan, direk ne yazıyorsa onu verir.
+
+export const StateProvider = ({ reducer, initialState, children }) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
+// This is how we use it inside of a component
+export const useStateValue = () => useContext(StateContext);
