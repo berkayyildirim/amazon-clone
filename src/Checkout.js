d1137c47d64ed541f6a2b9d56217874d7319c9e4
berkayyildirim <berkay.yildirim@yahoo.com>
2021-03-27 12:05:23 +0300

    complete checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -8,7 +8,8 @@ function Checkout() {
     
     return (
         <div className="checkout">
-             <img 
+            <div className="checkout__left">
+            <img 
                 className="checkout__ad" 
                 src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
                 alt="" 
@@ -26,13 +27,20 @@ function Checkout() {
                     {/* List out all of the Checkout Products */}
                     {basket?.map(item => (
                         <CheckoutProduct 
-                            item={item.id}
+                            id={item.id}
                             title={item.title}
                             image={item.image}
                             price={item.price}
                             rating={item.rating}
                         />
                     ))}
+                </div>
+            )}
+            </div>
+            {basket.length > 0 && (
+                <div className="checkout__right">
+                    <h1>Subtotal</h1>
+                    {/* <Subtotal /> */}
                 </div>
             )}
         </div>
--- src/reducer.js
+++ src/reducer.js
@@ -20,7 +20,32 @@ const reducer = (state, action) => {
             };
         case "REMOVE_FROM_BASKET":
             // Logic for Removing item from basket
-            return { state };
+            
+            // we cloned the basket
+            let newBasket = [...state.basket];
+
+            
+            // This part will always be copied to another projects, therefore please do not memorize it, just copy/paste it.
+            // we check to see if product exists,
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+                );
+
+            if (index >= 0) {
+                // item exists in basket, remove it...
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Cant remove product (id: ${action.id}) as its not in basket`
+                );
+            }
+            
+
+
+            return { 
+                ...state, 
+                basket: newBasket,
+            };
         default:
             return state;
     }
